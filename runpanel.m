233bc6101048858716c9c2cc1beec57b82c81fe2
Maya <100138818+mxyx00@users.noreply.github.com>
2023-10-16 11:00:48 -0700

    Update runpanel.m

--- runpanel.m
+++ runpanel.m
@@ -1,8 +1,8 @@
 %% panelcode(airfoil, angle of attack (º), airflow velocity (m/s), resolution)
 
 figure()
-panelcode("naca633618.dat",10,2,10) % Low Res
-figure()
 panelcode("naca633618.dat",10,2,50) % High Res
 figure()
 panelcode("naca633618.dat",-10,2,50) % Downward aoa, displaying pressure variation 
+figure()
+panelcode("n0012.dat", 0,2,50) % Symmetrical airfoil
